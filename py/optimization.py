c60e6bfb8759c6546e3f632c1b5a7cd07c6a27c0
Inaba Naoki <inabanaoki-2@monfalcone-2.local>
2024-04-19 14:08:27 +0900

    write an optimizer to normal gradient descent for two parameter (vector)

---
+++ py/optimization.py
@@ -0,0 +1,65 @@
+import cirq
+import openfermion
+import numpy as np
+import datetime
+from anzats import Anzats
+# from expectation import get_expectation_ZiZj, get_expectation_ghz_l4, get_expectation_ghz_l8
+# from optimization import get_gradient, optimize_by_gradient_descent
+
+def get_gradient(function, gamma: np.array, beta: np.array, delta_gamma, delta_beta, iter):
+    grad_gamma = np.zeros_like(gamma)
+    grad_beta  = np.zeros_like(beta)
+    gamma_edge = gamma
+    beta_edge  = beta
+    # initial gamma, beta?
+    
+    if not (gamma.size == beta.size):
+        return 1
+
+    for index in range(gamma.size):
+        center = gamma[index]
+        gamma_edge[index] = gamma[index] - delta_gamma
+        e1 = function(gamma_edge, beta)
+        gamma_edge[index] = gamma[index] + delta_gamma
+        e2 = function(gamma_edge, beta)
+        grad_gamma[index] = (e2.real-e1.real)/(2*delta_gamma)
+        gamma[index] = center
+
+        center = beta[index]
+        beta_edge[index] = beta[index] - delta_beta
+        e1 = function(gamma, beta_edge)
+        beta_edge[index] = beta[index] + delta_beta
+        e2 = function(gamma, beta_edge)
+        grad_beta[index] = (e2.real-e1.real)/(2*delta_beta)
+        beta[index] = center
+    
+    return grad_gamma, grad_beta
+
+
+def optimize_by_gradient_descent(function, initial_gamma: np.array, initial_beta: np.array, alpha, delta_gamma, delta_beta, iteration, Figure=True,filepath=""):
+    gamma, beta = initial_gamma, initial_beta
+
+    textlines = []
+    textlines.append("iter, energy, gamma[0], beta[0],...")
+    print("iter, energy, gamma[0], beta[0],...")
+
+    for iter in range(int(iteration)):
+        # it is complex for me to set get_gradient for two optical parameter_vector
+        grad_gamma, grad_beta = get_gradient(function, gamma, beta, delta_gamma, delta_beta, iter)
+        gamma -= alpha * grad_gamma
+        beta  -= alpha * grad_beta
+        energy = function(gamma, beta)
+
+        record = [iter, energy]
+        for index in range(gamma.size):
+            record.append(gamma[index])
+            record.append(beta[index])
+        textlines.append(record)
+        print(record)
+    
+    if len(filepath)>0:
+        with open(filepath, mode='a') as f:
+            for i, textline in enumerate(textlines):
+                f.write("{}\n".format(textline))
+
+    return gamma, beta
