89b005590c4a763b07dc8920c0ba4de0c634f930
inavert <124209126+inavert@users.noreply.github.com>
2024-04-22 06:16:45 +0900

    add main file to output csvs and params for different length and p

---
+++ py/4-1_ising.py
@@ -0,0 +1,56 @@
+import os 
+import sys
+import datetime
+
+import cirq
+import openfermion
+import numpy as np
+from anzats import Anzats
+from expectation import *
+from optimization import get_gradient, optimize_by_gradient_descent
+
+def main():
+    length_list = [8,10,12,14,16]
+    alpha = 0.01
+    delta_gamma = 0.001
+    delta_beta  = 0.001
+    iteration = 30
+    results_dir_path = os.path.join('.results')
+    if not os.path.exists(results_dir_path):
+        os.mkdir(results_dir_path)
+
+    for length in length_list:
+        for p in range(1,int(length/2)+1):
+            # length = 10
+            # p = 5
+            initial_gamma = np.array([(0.5) for i in range(p)])
+            initial_beta  = np.array([(0.5) for i in range(p)])
+            
+            t_delta = datetime.timedelta(hours=9)
+            JST = datetime.timezone(t_delta, 'JST')
+            now = datetime.datetime.now(JST)
+            ymdhms = now.strftime('%Y-%m-%d_%H-%M-%S')
+            csvpath = os.path.join(results_dir_path, '4-1_ising_l{:02}_p{}_{}.csv'.format(length, p, ymdhms))
+            gamma, beta = optimize_by_gradient_descent(eval("get_expectation_ghz_l{}".format(length)),initial_gamma,initial_beta,alpha,delta_gamma,delta_beta ,iteration,True,csvpath)
+
+            print(gamma, beta)
+
+
+            t_delta = datetime.timedelta(hours=9)
+            JST = datetime.timezone(t_delta, 'JST')
+            now = datetime.datetime.now(JST)
+            ymdhms = now.strftime('%Y-%m-%d_%H-%M-%S')
+            tomlpath = os.path.join(results_dir_path, '4-1_ising_l{:02}_p{}_{}.toml'.format(length, p, ymdhms))
+
+            with open(tomlpath, mode='a') as f:
+                f.write("length       ={}\n".format(length))
+                f.write("p            ={}\n".format(p))
+                f.write("alpha        ={}\n".format(alpha))
+                f.write("initial_gamma={}\n".format(initial_gamma))
+                f.write("initial_beta ={}\n".format(initial_beta))
+                f.write("delta_gamma  ={}\n".format(delta_gamma))
+                f.write("delta_beta   ={}\n".format(delta_beta))
+                f.write("iteration    ={}\n".format(iteration))
+
+if __name__=='__main__':
+    main()
