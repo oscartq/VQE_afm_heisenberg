7498ff5951e598e932e4dc01cb860c1e2cdcdeea
inavert <124209126+inavert@users.noreply.github.com>
2024-04-22 18:34:33 +0900

    create vqcs_spin_systems/py/4-2_critical-state.py

---
+++ py/4-2_critical-state.py
@@ -0,0 +1,28 @@
+import cirq
+import openfermion
+import numpy as np
+import datetime
+from anzats import Anzats
+from expectation import get_expectation_critical_state, TFIMStateArgs
+from optimization import optimize_by_gradient_descent
+from functools import partial
+Pi=3.1415
+
+length = 8
+p = 4
+initial_gamma = np.array([0.5 for i in range(p)])
+initial_beta  = np.array([0.5 for i in range(p)])
+g = 1.0
+function_args = TFIMStateArgs(length, g)
+
+iteration = 10
+alpha = 0.01
+delta_gamma = 0.001
+delta_beta  = 0.001
+
+print("exact energy: ")
+print(-1.28*length)
+
+gamma, beta = optimize_by_gradient_descent(partial(get_expectation_critical_state, function_args=function_args), initial_gamma, initial_beta, alpha, delta_gamma, delta_beta, iteration)
+
+print(gamma, beta)
