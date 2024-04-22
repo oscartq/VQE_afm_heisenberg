fe34dd9e3e56fe5203d65efa9c37a40d1cac47ff
inavert <124209126+inavert@users.noreply.github.com>
2024-04-22 18:30:23 +0900

    add TFIM expectation function and args class

--- py/expectation.py
+++ py/expectation.py
@@ -6,6 +6,32 @@ from anzats import Anzats
 # from expectation import get_expectation_ZiZj, get_expectation_ghz_l4, get_expectation_ghz_l8
 # from optimization import get_gradient, optimize_by_gradient_descent
 
+class TFIMStateArgs():
+    def __init__(self, length, g):
+        self.length = length
+        self.g = g
+
+
+def get_expectation_critical_state(function_args, gamma, beta):
+    anzats = Anzats(function_args.length, gamma, beta)
+    circuit = anzats.circuit
+    qubits = anzats.qubits
+    vector = cirq.final_state_vector(circuit)
+
+    value = 0 + 0j
+    for i in range(function_args.length):
+        circuit = anzats.circuit.copy()
+        circuit.append(cirq.Z(qubits[i]))
+        circuit.append(cirq.Z(qubits[(i+1)%function_args.length]))
+        vector2 = cirq.final_state_vector(circuit)
+        value -= np.dot(vector2.conj(), vector)
+    for i in range(function_args.length):
+        circuit = anzats.circuit.copy()
+        circuit.append(function_args.g * cirq.X(qubits[i]))
+        vector2 = cirq.final_state_vector(circuit)
+        value -= np.dot(vector2.conj(), vector)
+    return value
+
 def get_expectation_ghz(length, gamma, beta):
     anzats = Anzats(length, gamma, beta)
     circuit = anzats.circuit
