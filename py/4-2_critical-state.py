76a23ed6e8969f2f7e60103a08fcf6fc00ebba24
inavert <124209126+inavert@users.noreply.github.com>
2024-05-01 01:09:04 +0900

    let optimizer using multucore

--- py/4-2_critical-state.py
+++ py/4-2_critical-state.py
@@ -2,54 +2,17 @@ import os
 import datetime
 from functools import partial
 import tomllib
+import multiprocessing as mp
 
 import cirq
 import openfermion
 import numpy as np
 from anzats import Anzats
-from expectation import get_expectation_critical_state, TFIMStateArgs
-from optimization import optimize_by_gradient_descent, optimize_by_gradient_descent_gpu
+from expectation import get_expectation_critical_state, get_expectation_critical_state_gpu, TFIMStateArgs
+from optimization import optimize_by_gradient_descent, optimize_by_gradient_descent_gpu, optimize_by_gradient_descent_multiprocess
 
 Pi=3.1415
 
-
-def optimize_critical_state(length, 
-                            g,
-                            qsim_option, 
-                            p, 
-                            alpha, 
-                            delta_gamma, 
-                            delta_beta, 
-                            initial_gamma,
-                            initial_beta,
-                            iteration, 
-                            csvpath, 
-                            tomlpath,
-                            gpu=False):
-
-    function_args = TFIMStateArgs(length, g, qsim_option)
-
-    print("exact energy: ")
-    print(-1.28*length)
-
-    if gpu:
-        gamma, beta = optimize_by_gradient_descent_gpu(partial(get_expectation_critical_state, function_args=function_args), initial_gamma, initial_beta, alpha, delta_gamma, delta_beta, iteration, True, csvpath)
-
-    else:
-        gamma, beta = optimize_by_gradient_descent(partial(get_expectation_critical_state, function_args=function_args), initial_gamma, initial_beta, alpha, delta_gamma, delta_beta, iteration, True, csvpath)
-    print(gamma, beta)
-
-
-    with open(tomlpath, mode='a') as f:
-        f.write("length       ={}\n".format(length))
-        f.write("p            ={}\n".format(p))
-        f.write("g            ={}\n".format(g))
-        f.write("alpha        ={}\n".format(alpha))
-        f.write("initial_gamma={}\n".format("["+", ".join(str(value) for i, value in enumerate(initial_gamma.tolist()))+"]"))
-        f.write("initial_beta ={}\n".format("["+", ".join(str(value) for i, value in enumerate(initial_beta.tolist())) +"]"))
-        f.write("delta_gamma  ={}\n".format(delta_gamma))
-        f.write("delta_beta   ={}\n".format(delta_beta))
-        f.write("iteration    ={}\n".format(iteration))
 
 def main():
     with open(".toml", mode="rb") as f:
@@ -60,16 +23,11 @@ def main():
     g = config["critical_state"]["g"]
     alpha = config["critical_state"]["alpha"]
     delta_gamma = config["critical_state"]["delta_gamma"]
-    delta_beta  = config["critical_state"]["delta_beta"]
-    initial_gamma = np.array(config["critical_state"]["initial_gamma"])
-    initial_beta  = np.array(config["critical_state"]["initial_beta"])
+    delta_beta = config["critical_state"]["delta_beta"]
     iteration = config["critical_state"]["iteration"]
     gpu = config["critical_state"]["gpu"]
+    output_file_prefix = "4-2_critical"
 
-    if not (len(initial_gamma)==p_list[0]):
-        raise ValueError("length of the initial parameter gamma must equal to p")
-    if not (len(initial_beta)==p_list[0]):
-        raise ValueError("length of the initial parameter beta must equal to p")
         
     results_dir_path = config["critical_state"]["results_dir_path"]
     # results_dir_path = os.path.join('.results')
@@ -81,27 +39,50 @@ def main():
     ymdhms = now.strftime('%Y-%m-%d_%H-%M-%S')
 
 
+
+    # too much processes may cause delay because of communication between them
+    pool = mp.Pool(2)
+
     for p in p_list:
-        if not (len(initial_gamma)==p):
-            initial_gamma = np.array([0.5 for i in range(p)])
-            initial_beta  = np.array([0.5 for i in range(p)])
+        initial_gamma = np.array([0.5 for i in range(p)])
+        initial_beta  = np.array([0.5 for i in range(p)])
         for length in length_list:
             qsim_option = {'t': int(length/2), 'f':1}
-            csvpath = os.path.join(results_dir_path, '4-2_critical_l{:02}_p{}_{}.csv'.format(length, p, ymdhms))
-            tomlpath = os.path.join(results_dir_path, '4-2_critical_l{:02}_p{}_{}.toml'.format(length, p, ymdhms))
-            optimize_critical_state(length, 
-                            g,
-                            p, 
-                            qsim_option, 
-                            alpha, 
-                            delta_gamma, 
-                            delta_beta, 
-                            initial_gamma,
-                            initial_beta,
-                            iteration, 
-                            csvpath, 
-                            tomlpath,
-                            gpu)
+            csvpath = os.path.join(results_dir_path, '{}_l{:02}_p{}_{}.csv'.format(output_file_prefix, length, p, ymdhms))
+            tomlpath = os.path.join(results_dir_path, '{}_l{:02}_p{}_{}.toml'.format(output_file_prefix, length, p, ymdhms))
+
+            
+            with open(tomlpath, mode='a') as f:
+                f.write("length       ={}\n".format(length))
+                f.write("p            ={}\n".format(p))
+                f.write("g            ={}\n".format(g))
+                f.write("alpha        ={}\n".format(alpha))
+                f.write("initial_gamma={}\n".format("["+", ".join(str(value) for i, value in enumerate(initial_gamma.tolist()))+"]"))
+                f.write("initial_beta ={}\n".format("["+", ".join(str(value) for i, value in enumerate(initial_beta.tolist())) +"]"))
+                f.write("delta_gamma  ={}\n".format(delta_gamma))
+                f.write("delta_beta   ={}\n".format(delta_beta))
+                f.write("iteration    ={}\n".format(iteration))
+
+            function_args = TFIMStateArgs(length, g, qsim_option)
+
+            print("exact energy: ")
+            print(-1.28*length)
+
+            gamma, beta = optimize_by_gradient_descent_multiprocess(
+                function=partial(get_expectation_critical_state_gpu, function_args=function_args), 
+                initial_gamma=initial_gamma, 
+                initial_beta=initial_beta, 
+                alpha=alpha, 
+                delta_gamma=delta_gamma, 
+                delta_beta=delta_beta, 
+                iteration=iteration, 
+                figure=True,
+                filepath=csvpath, 
+                pool=pool)
+            print(gamma, beta)
+
+    pool.close()
+    pool.join()
 
 if __name__=='__main__':
     main()
--- py/optimization.py
+++ py/optimization.py
@@ -2,6 +2,7 @@ import os
 import sys
 import datetime
 import csv 
+import multiprocessing as mp
 
 import cirq
 import openfermion
@@ -144,78 +145,56 @@ def optimize_by_gradient_descent_gpu(function, initial_gamma: np.array, initial_
     return gamma, beta
 
 
-def optimize_by_gradient_descent_multiprocess(function, initial_gamma: np.array, initial_beta: np.array, alpha, delta_gamma, delta_beta, iteration, Figure=True,filepath=""):
-    gamma, beta = np.asarray(initial_gamma), np.asarray(initial_beta)
+def gradient_parallel(pool, f, gamma, beta, h=1e-5):
+    if not (gamma.size == beta.size):
+        return None  # ベクトルサイズが一致しなければNoneを返す
 
-    textlines = []
-    headline = ["iter", "energy"]
-    for p in range(int(len(initial_gamma))):
-        headline.append("gamma[{}]".format(p))
-        headline.append("bata[{}]".format(p))
-    print(headline)
-    textlines.append(headline)
+    n = gamma.size
+    args_gamma = [(f, gamma, beta, i, h, 'gamma') for i in range(n)]
+    args_beta = [(f, gamma, beta, i, h, 'beta') for i in range(n)]
+    grad_gamma = pool.starmap(partial_derivative_gamma, args_gamma)
+    grad_beta = pool.starmap(partial_derivative_beta, args_beta)
+    # pool.join()
+    return np.array(grad_gamma), np.array(grad_beta)
 
-    for iter in range(int(iteration)):
-        # it is complex for me to set get_gradient for two optical parameter_vector
-        grad_gamma, grad_beta = get_gradient_multiprocess(function, gamma, beta, delta_gamma, delta_beta, iter)
-        gamma -= alpha * grad_gamma
-        beta  -= alpha * grad_beta
-        energy = function(gamma=gamma, beta=beta)
+def partial_derivative_gamma(f, gamma, beta, i, h=1e-5, variable='gamma'):
+    var_plus = np.array(gamma, dtype=float)
+    var_minus = np.array(var_plus, dtype=float)
+    var_plus[i] += h
+    var_minus[i] -= h
+    derivative = (f(gamma=var_plus, beta=beta).real - f(gamma=var_minus, beta=beta).real) / (2 * h)
+    return derivative
 
-        record = [iter, energy]
-        for index in range(gamma.size):
-            record.append(gamma[index])
-            record.append(beta[index])
-        textlines.append(record)
-        print(record)
-    
-    if len(filepath)>0:
-        with open(filepath, mode='a') as f:
-            writer = csv.writer(f)
-            for i, textline in enumerate(textlines):
-                writer.writerow(textline)
-                # f.write("{}\n".format(textline))
+def partial_derivative_beta(f, gamma, beta, i, h=1e-5, variable='gamma'):
+    var_plus = np.array(beta, dtype=float)
+    var_minus = np.array(var_plus, dtype=float)
+    var_plus[i] += h
+    var_minus[i] -= h
+    derivative = (f(gamma=gamma, beta=var_plus).real - f(gamma=gamma, beta=var_minus).real) / (2 * h)
+    return derivative
+
+def optimize_by_gradient_descent_multiprocess(function, initial_gamma, initial_beta, alpha, delta_gamma, delta_beta, iteration, figure=True, filepath="", pool=mp.Pool(2)):
+    gamma, beta = initial_gamma.copy(), initial_beta.copy()
+
+    with open(filepath, mode='a', newline='') as f:
+        writer = csv.writer(f)
+        headline = ["iter", "energy"]
+        for p in range(int(len(initial_gamma))):
+            headline.append("gamma[{}]".format(p))
+            headline.append("bata[{}]".format(p))
+        print(headline)
+        writer.writerow(headline)
+
+        for iter in range(iteration):
+            grad_gamma, grad_beta = gradient_parallel(pool, function, gamma, beta, delta_gamma)
+            # print(grad_beta, grad_gamma)
+            gamma -= alpha * grad_gamma
+            beta -= alpha * grad_beta
+            energy = function(gamma=gamma, beta=beta)
+
+            record = [iter, energy] + [val for pair in zip(gamma, beta) for val in pair]
+            writer.writerow(record)
+            if figure:
+                print(record)
 
     return gamma, beta
-
-def get_gradient_multiprocess(function, gamma: np.array, beta: np.array, delta_gamma, delta_beta, iter):
-    grad_gamma = np.zeros_like(gamma)
-    grad_beta  = np.zeros_like(beta)
-    gamma_edge = gamma
-    beta_edge  = beta
-    # initial gamma, beta?
-    
-    if not (gamma.size == beta.size):
-        return 1
-
-
-
-    tasks = []
-    job_list = []
-    pipe_list = []
-    p = Pool(4) # プロセス数を4に設定
-
-    for index in range(gamma.size):
-        get_rev,send_rev  = Pipe(False)
-        center = gamma[index]
-        gamma_edge[index] = gamma[index] - delta_gamma
-        p1 = p.apply_async(function, args=[gamma_edge, beta])
-        gamma_edge[index] = gamma[index] + delta_gamma
-        p2 = p.apply_async(function, args=[gamma_edge, beta])
-        gamma[index] = center
-
-
-        center = beta[index]
-        beta_edge[index] = beta[index] - delta_beta
-        p3 = p.apply_async(function, args=[gamma, beta_edge])
-        beta_edge[index] = beta[index] + delta_beta
-        p4 = p.apply_async(function, args=[gamma, beta_edge])
-
-        grad_gamma[index] = (p2.get().real-p1.get().real)/(2*delta_gamma)
-        grad_beta[index] = (p4.get().real-p3.get().real)/(2*delta_beta)
-        beta[index] = center
-    
-    p.close()
-
-    return grad_gamma, grad_beta
-
