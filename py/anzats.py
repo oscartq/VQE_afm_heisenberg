5c0f44b4e0f0761569f87a7ce7a5e5c729b20af5
inavert <124209126+inavert@users.noreply.github.com>
2024-05-08 11:18:59 +0900

    AnzatsBCSHubbard

--- py/anzats.py
+++ py/anzats.py
@@ -1,7 +1,8 @@
 import cirq
-import openfermion
+import openfermion as of
 import numpy as np
 import datetime
+from inner_functions import _get_one_body_term_on_hubbard, _get_two_body_term_on_hubbard, _exponentiate_quad_ham
 # from anzats import Anzats
 # from expectation import get_expectation_ZiZj, get_expectation_ghz_l4, get_expectation_ghz_l8
 # from optimization import get_gradient, optimize_by_gradient_descent
@@ -136,3 +137,58 @@ class AnzatsToricCode():
         self.qubits = qubits
         self.gamma = gamma
         self.beta = beta
+
+
+class AnzatsBCSHubbard():
+    def __init__(self, function_args, gamma, beta):
+        # initialize circuit
+        x = function_args.x_dimension
+        y = function_args.y_dimension
+        n_sites = function_args.n_sites
+        tunneling=function_args.tunneling
+        hopping_matrix=function_args.hopping_matrix
+        coulomb=function_args.coulomb
+        periodic=function_args.periodic
+        sc_gap=function_args.sc_gap
+        n_qubits = function_args.n_qubits
+        qubits = cirq.LineQubit.range(n_qubits)
+        
+        # |\psi 1> circuit
+        mean_field_model = of.mean_field_dwave(
+            x, y, tunneling, sc_gap, periodic)
+        quad_ham = of.get_quadratic_hamiltonian(mean_field_model)
+        
+        circuit = cirq.Circuit(
+            of.circuits.prepare_gaussian_state(qubits, quad_ham))
+
+        for index in range(gamma.size):
+            # H2 (gamma) hubbard
+            # circuit tunneling
+            for term in _get_one_body_term_on_hubbard(hopping_matrix, n_sites):
+                term_qubit = of.qubit_operator_to_pauli_sum(of.jordan_wigner(term))
+                circuit.append(cirq.PauliSumExponential(
+                    term_qubit,
+                    exponent=gamma[index],
+                    atol = 1e-08
+                ))
+
+            # circuit coulomb
+            for term in _get_two_body_term_on_hubbard(coulomb, n_sites, n_qubits):
+                term_qubit = of.qubit_operator_to_pauli_sum(of.jordan_wigner(term))
+                circuit.append(cirq.PauliSumExponential(
+                    term_qubit,
+                    exponent=gamma[index],
+                    atol = 1e-08
+                ))
+
+            # H1 (beta) bcs 
+            circuit.append(
+                cirq.Circuit(
+                    _exponentiate_quad_ham(qubits, quad_ham, beta[index]
+            )))
+
+        self.circuit = circuit
+        self.qubits = qubits
+        self.gamma = gamma
+        self.beta = beta
+
